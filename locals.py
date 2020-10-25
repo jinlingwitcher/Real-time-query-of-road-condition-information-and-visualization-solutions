68e60ead39e44c175025516f15925a082f826b5d
jinlingwitcher <41133466+jinlingwitcher@users.noreply.github.com>
2020-10-25 15:30:30 +0800

    Update locals.py

--- locals.py
+++ locals.py
@@ -4,12 +4,11 @@ import requests
 from datetime import datetime
 import time
 
-f=open(r'C:\Users\newlife\Desktop\sql.txt','a+')
-datatime = datetime.now().strftime("%Y-%m-%d %H_%M_%S")
-
+nums=0
 try:
 	while True:
-
+		f=open(r'C:\Users\newlife\Desktop\c.txt','a+')
+		datatime = datetime.now().strftime("%Y-%m-%d %H_%M_%S")
 		queryStr2 = 'https://restapi.amap.com/v3/weather/weatherInfo?city=320100&key=yourkey'
 		encodedStr2 = urllib.parse.quote(queryStr2, safe="/:=&?#+!$,;'@()*[]")
 		w=requests.get(encodedStr2)
@@ -38,20 +37,22 @@ try:
 		f.write(w[b4+13])
 		f.write(' ')
 		f.write('\n')
-
+		print('weather done!')
 
 
 		locals=[]
-		s1=118.776767 #云南路
-		s2=32.048555
-		for i in range(3):
-			for j in range(3):
-				ss1=str(s1+i*0.000972)
-				ss2=str(s2+j*0.00064025)
+		s1=118.773164 #南京医科大学左上角 
+		s2=32.045922
+		n=0
+		for i in range(30):
+			for j in range(30):
+				ss2=str(s2-i*0.001190)
+				ss1=str(s1+j*0.001190)
 				ss3=ss1+','+ss2
-				locals.append(ss3)			
+				locals.append(ss3)		#118.823181	
+		print('locals are ready')
 		for local in locals:
-			queryStr1 = 'https://restapi.amap.com/v3/traffic/status/circle?location='+local+'&radius=50&key=yourkey'
+			queryStr1 = 'https://restapi.amap.com/v3/traffic/status/circle?location='+local+'&radius=40&key=yourkey'
 			encodedStr1 = urllib.parse.quote(queryStr1, safe="/:=&?#+!$,;'@()*[]")
 			r=requests.get(encodedStr1)
 			r=r.text
@@ -60,16 +61,19 @@ try:
 				f.write('0')
 			else:
 				f.write(r[a+9])
-			f.write(' ')
-			f.write(local)
-			f.write('\n')
+			n=n+1
+			print(n)
+			if n==30:
+				f.write('\n')
+				n=0
+				continue
+			f.write(',')
+		f.close()
+		print (time.strftime('%H:%M:%S',time.localtime(time.time())))
 		print('Succeed!')
-		time.sleep(2)
-	
+		nums=nums+1
+		if nums==33:#每20min查一次，一共33次，连续11h
+			break
+		time.sleep(1200)
 except KeyboardInterrupt:
 	print('finished!')
-f.close()	
-
-
-
-
